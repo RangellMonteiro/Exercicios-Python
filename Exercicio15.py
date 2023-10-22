6dff5785a2160d267dfe17800a67268d53be1cc6
RangellMonteiro <rangell_monteiro@hotmail.com>
2023-10-22 00:23:47 -0300

    Exercicios Python parte 1

--- Exercicio15.py
+++ Exercicio15.py
@@ -1,2 +1,4 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+import math
 valor = float(input("Digite um valor: "))
+print("O valor digitado foi {} e sua porção inteira é {}".format(valor, math.trunc(valor)))
